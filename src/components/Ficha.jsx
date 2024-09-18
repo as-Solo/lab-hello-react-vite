8b468a0bec02665f0619f6f96ac6b86fa6a2f8ae
as-Solo <asdelsolo@gmail.com>
2024-09-18 12:36:48 +0200

    backup

--- src/components/Ficha.jsx
+++ src/components/Ficha.jsx
@@ -1,7 +1,13 @@
-function Ficha(){
-    return (
-
-    )
-}
-
-export default Ficha
+// function Ficha({ image, title, description }) {
+//     return (
+//       <div className="ficha">
+//         <img src={image} alt={title} />
+//         <div className="texto">
+//           <h3>{title}</h3>
+//           <p>{description}</p>
+//         </div>
+//       </div>
+//     );
+//   }
+  
+//   export default Ficha;
